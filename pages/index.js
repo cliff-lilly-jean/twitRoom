4858c83329f31055c3b982ae46d3e1652586183f
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-01-05 17:50:45 -0500

    caught up

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import Sidebar from '../components/Sidebar';
 
 export default function Home() {
  return (
-  // Had to remove the flex class, for some reason it was causing the main
-  // bg of black not to show up
+  // Had to remove the flex class on the wrapper container, for some reason
+  // it was causing the main bg of black not to show up
 
   // Also removed the class py-2 from the wrapper container; padding top and bottom of 5px
   // because it was causing a white border at the top and bottom of the page
