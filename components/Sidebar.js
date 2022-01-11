08e88e94fd153c0a57796bff757d6be9a9bf97a9
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-01-11 05:54:48 -0500

    completed the sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -29,6 +29,15 @@ function Sidebar() {
     <SidebarLink text="Profile" Icon={UserIcon} />
     <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
    </div>
+   <button className='hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]'>Tweet</button>
+   <div className='text-[#d9d9d9] flex items-center justify-center hoverAnimation mt-auto xl:ml-auto xl:-mr-5'>
+    <img className='h-10 w-10 rounded-full xl:mr-2.5' src="" alt="" />
+    <div className='hidden xl:inline leading-5'>
+     <h4 className='font-bold'>Firebase User </h4>
+     <p className='text-[#6e767d]'>@Jeanius001</p>
+    </div>
+    <DotsHorizontalIcon className="h-5 hidden xl:inline ml-10" />
+   </div>
   </div>
  );
 }
